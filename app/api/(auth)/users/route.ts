faad6fd0d45e9845fbe9a59896ec08c0a057824c
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2024-06-22 14:26:28 +0000

    Add DELETE method to users API.

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -63,3 +63,38 @@ export const PATCH = async(request: Request) => {
     })
   }
 }
+
+export const DELETE = async(request: Request) => {
+  try {
+    await connect();
+    const body = await request.json();
+    const { _id } = body
+    if (!_id) {
+      return new NextResponse('ID not found', {
+        status: 400
+      })
+    }
+
+    if (!Types.ObjectId.isValid(_id)) {
+      return new NextResponse('Invalid ID', {
+        status: 400
+      })
+    }
+
+    const user = await User.findByIdAndDelete(_id);
+
+    if (!user) {
+      return new NextResponse('User not found', {
+        status: 404
+      })
+    }
+    
+    return new NextResponse(JSON.stringify(user), {
+      status: 200
+    })
+  } catch (error: any) {
+    return new NextResponse('Error deleting user ' + error.message, {
+      status: 500
+    })
+  }
+}
