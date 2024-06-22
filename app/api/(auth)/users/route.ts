2eb1efe9d1c3cbc3c25629146839685e4f76fa94
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2024-06-22 13:41:19 +0000

    Fetch user data.

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,18 @@
 import { NextResponse } from 'next/server'
+import connect from '@/lib/db'
+import User from '@/lib/modals/user'
  
-export const GET = (request: Request) => {
-  return new NextResponse('Hello, Next.js!')
+export const GET = async(request: Request) => {
+  try {
+    await connect();
+    const users = await User.find();
+    
+    return new NextResponse(JSON.stringify(users), {
+      status: 200
+    })
+  } catch (error: any) {
+    return new NextResponse('Error fetching users ' + error.message, {
+      status: 500
+    })
+  }
 }
