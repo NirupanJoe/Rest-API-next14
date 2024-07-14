da876245895154018a7dcd814d34413b29ed5563
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2024-07-14 12:15:11 +0000

    Added the POST method to categories API.

--- app/api/(dashboard)/categories/route.ts
+++ app/api/(dashboard)/categories/route.ts
@@ -32,3 +32,34 @@ export const GET = async(request: Request) => {
         })
     }
 }
+
+export const POST = async(request: Request) => {
+    try {
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get('userId');
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse('Invalid user ID', {
+                status: 400
+            })
+        }
+        const { title } = await request.json();
+
+        await connect();
+
+        const userExists = await User.findById(userId);
+        if (!userExists) {
+            return new NextResponse('User not found in the database', {
+                status: 404
+            })
+        }
+
+        const category = await Category.create({ user: new Types.ObjectId(userId), title });
+        return new NextResponse(JSON.stringify(category), {
+            status: 201
+        })
+    } catch (error: any) {
+        return new NextResponse('Error creating category ' + error.message, {
+            status: 500
+        })
+    }
+}
