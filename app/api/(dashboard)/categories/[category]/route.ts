db8a424e72935c68b6ffad4e2776631a10d195bd
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2024-07-17 16:17:28 +0000

    Created the DELETE for category.

--- app/api/(dashboard)/categories/[category]/route.ts
+++ app/api/(dashboard)/categories/[category]/route.ts
@@ -45,3 +45,42 @@ export const PATCH = async(request: Request, context: { params: any }) => {
         })
     }
 }
+
+export const DELETE = async(request: Request, context: { params: any }) => {
+    const categoryId = context.params.category;
+    try {
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get('userId');
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse('Invalid user ID', {
+                status: 400
+            })
+        }
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+            return new NextResponse('Invalid category ID', {
+                status: 400
+            })
+        }
+        await connect();
+        const userExists = await User.findById(userId);
+        if (!userExists) {
+            return new NextResponse('User not found in the database', {
+                status: 404
+            })
+        }
+        const categoryExists = await Category.findOne({ _id: categoryId, user: userId });
+        if (!categoryExists) {
+            return new NextResponse('Category not found in the database', {
+                status: 404
+            })
+        }
+        const category = await Category.findByIdAndDelete(categoryId);
+        return new NextResponse(JSON.stringify({ message: "Category deleted", category }), {
+            status: 200
+        })
+    } catch (error: any) {  
+        return new NextResponse('Error deleting category ' + error.message, {
+            status: 500
+        })
+    }
+}
