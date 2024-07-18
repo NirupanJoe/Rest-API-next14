89284c9e2b99304fcc9ea58164ad19567128d365
Nirupan Joe <84219089+NirupanJoe@users.noreply.github.com>
2024-07-18 17:05:19 +0000

    Created the blog POST.

--- app/api/(dashboard)/blog/route.ts
+++ app/api/(dashboard)/blog/route.ts
@@ -57,3 +57,58 @@ export const GET = async(request: Request) => {
         })
     }
 }
+
+export const POST = async(request: Request) => {
+    try {
+        const body = await request.json();
+        const { title, description } = body;
+        
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get('userId');
+
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse('Invalid user ID', {
+                status: 400
+            })
+        }
+
+        const categoryId = searchParams.get('categoryId');
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+            return new NextResponse('Invalid category ID', {
+                status: 400
+            })
+        }
+
+        await connect();
+
+        const userExists = await User.findById(userId);
+        if (!userExists) {
+            return new NextResponse('User not found in the database', {
+                status: 404
+            })
+        }        
+        const categoryExists = await Category.findById(categoryId);
+        if (!categoryExists) {
+            return new NextResponse('Category not found in the database', {
+                status: 404
+            })
+        }
+
+        const newBlog = new Blog({
+            title,
+            description,
+            user: new Types.ObjectId(userId),
+            category: new Types.ObjectId(categoryId)
+        })
+
+        const blog = await newBlog.save();
+
+        return new NextResponse(JSON.stringify(blog), {
+            status: 201
+        })
+    } catch (error: any) {
+        return new NextResponse('Error creating blog post ' + error.message, {
+            status: 500
+        })
+    }
+}
